5c15bc4d05381b303e16a698708e6849587b1960
FernandoFeo <feofer17@gmail.com>
2024-02-29 17:36:04 -0300

    Segunda-pre

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavBar from './components/NavBar/NavBar'
 import ItemListConteiner from './components/ItemListConteiner/ItemListConteiner'
+import ItemCount from './components/ItemCount/ItemCount'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +14,7 @@ function App() {
     <>
       <NavBar/>
       <ItemListConteiner greeting ={ 'Bienvenidos a Distri Fer '} />
-      
+      <ItemCount initial={0} stock={30} onAdd={(Quantity )=> console.log('Cantidad agregada ', Quantity)} />
       
     </>
   )
--- src/components/ItemListConteiner/ItemListConteiner.jsx
+++ src/components/ItemListConteiner/ItemListConteiner.jsx
@@ -1,9 +1,29 @@
+import { useState,useEffect } from "react"
+import { getProducts } from "../../../asyncMock"
+import ItemList from "../ItemList/ItemList"
+
+
+
 const ItemListConteiner =( {greeting  } )=>{
+    const [products , setProducts] = useState( [ ] )
+    useEffect( ( ) => {
+        getProducts()
+        .then (response =>{
+             setProducts(response)
+        }
+        )
+        .catch (error => {
+            console.log (error)
+        })
+    } , [ ])
+
     return(
         <div> 
             <h1 style={ { textAlign:"center"}}> {greeting} </h1>
-            
+            <ItemList products ={ products} />
         </div>
+
+    
     )
 }
 
