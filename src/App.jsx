7aee29f94c3d6d022196cb4967074f4152751359
FernandoFeo <feofer17@gmail.com>
2024-04-02 21:03:39 -0300

    proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes , Route } from 'react-router-dom'
-import NavBar from './components/NavBar/NavBar'
-import ItemListConteiner from './components//ItemListConteiner/ItemListConteiner'
+import Navbar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListConteiner/ItemListConteiner'
 import ItemDetailContainer from './components/ItemDetailConteiner/ItemDetailConteiner'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartProvider } from './context/CartContext'
 import CartView from './components/CartView/CartView'
 import Checkout from './components/Checkout/Checkout'
-
-
 
 function App() {
  
@@ -17,10 +14,10 @@ function App() {
     <div>
      <BrowserRouter >
         <CartProvider> 
-          <NavBar/>
+          <Navbar/>
           <Routes > 
-            <Route path='/' element= {<ItemListConteiner greeting ={ 'Bienvenidos a Distri Fer '} />} > </Route>
-            <Route path='/category/:categoryId' element = {<ItemListConteiner greeting={ `Bienvenidos `}/>} />
+            <Route path='/' element= {<ItemListContainer greeting ={ 'Bienvenidos a Distri Fer '} />} > </Route>
+            <Route path='/category/:categoryId' element = {<ItemListContainer greeting={ `Bienvenidos `}/>} />
             <Route path='/item/:itemId' element = { <ItemDetailContainer/> }></Route>
             <Route path='/cart' element ={<CartView/>}></Route>
             <Route path='/Checkout' element ={ <Checkout/>} > </Route>
